3891b94c61692b4605070d44e3ea2cbc65ed9b1f
bowman <fancycode@gmail.com>
2018-05-14 10:25:06 +0800

    update license

--- contracts/DriftBottle.js
+++ contracts/DriftBottle.js
@@ -15,7 +15,31 @@
    / /\ \ \/  \/ / \___ \| |  | | |\/| |  __|                                                      
   / ____ \  /\  /  ____) | |__| | |  | | |____                                                     
  /_/    \_\/  \/  |_____/ \____/|_|  |_|______|                                                    
-                                                           
+                                                  
+ REGISTER WITH MY LINK License
+ Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+     and/or sell copies of the Software, and to permit persons to whom the 
+     Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+
+You should only use my inivtation link to register nebulas.io dapp 
+account and submit dapp in this account.
+
+The link is https://incentive.nebulas.io/signup.html?invite=9rX6a
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+  OR OTHER DEALINGS IN THE SOFTWARE.
+
 @author 梭哈人生 fancycode@gmail.com
  一个简单的漂流瓶智能合约
 合约初始化时需要传入所有者地址，以便操作withdraw函数进行转出
